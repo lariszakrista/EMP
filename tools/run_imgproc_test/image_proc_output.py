5c1f158aaae3b81b2574d39cb9c5042aa06a4d3a
George Harder <harderg@oregonstate.edu>
2017-04-19 18:32:41 +0000

    fix issue when a circle is not detected

--- tools/run_imgproc_test/image_proc_output.py
+++ tools/run_imgproc_test/image_proc_output.py
@@ -426,12 +426,12 @@ def read_metadata(original_path, processed_path, original_bucket, processed_buck
         item['comments'] = comments
         item['circles'] = circles
         
-        if tokens[1] is not None:
+        if tokens[1].startswith('c'):
             item['found_sun'] = literal_eval(tokens[1][1:])
         else:
             item['found_sun'] = "undefined"
             
-        if tokens[2] is not None:
+        if tokens[2].startswith('c'):
             item['found_moon'] = literal_eval(tokens[2][1:])
         else:
             item['found_sun'] = "undefined"
@@ -439,25 +439,33 @@ def read_metadata(original_path, processed_path, original_bucket, processed_buck
         if truth_file is not None and img_name in truth_positions:
  
             if truth_positions[img_name]['moon'] is not None:
-                moon_center_offset, moon_radius_diff = calc_position_diff(literal_eval(tokens[2][1:]), truth_positions[img_name]['moon'])
-                item['moon_center_diff'] = moon_center_offset
-                item['moon_rad_diff'] = moon_radius_diff
+                if tokens[2].startswith('c'):
+                    moon_center_offset, moon_radius_diff = calc_position_diff(literal_eval(tokens[2][1:]), truth_positions[img_name]['moon'])
+                    item['moon_center_diff'] = moon_center_offset
+                    item['moon_rad_diff'] = moon_radius_diff
+                else:
+                    item['moon_center_diff'] = "undefined"
+                    item['moon_rad_diff'] = "undefined"
             else:
                 item['moon_center_diff'] = "No Moon in ground truth"
                 item['moon_rad_diff'] = "No Moon in ground truth"
 
             if truth_positions[img_name]['sun'] is not None:
-                sun_center_offset, sun_radius_diff = calc_position_diff(literal_eval(tokens[1][1:]), truth_positions[img_name]['sun'])
-                item['sun_center_diff'] = sun_center_offset
-                item['sun_rad_diff'] = sun_radius_diff
+                if tokens[1].startswith('c'):
+                    sun_center_offset, sun_radius_diff = calc_position_diff(literal_eval(tokens[1][1:]), truth_positions[img_name]['sun'])
+                    item['sun_center_diff'] = sun_center_offset
+                    item['sun_rad_diff'] = sun_radius_diff
+                else:
+                    item['sun_center_diff'] = "undefined"
+                    item['sun_rad_diff'] = "undefined"
             else:
                 item['sun_center_diff'] = "No Sun in ground truth"
                 item['sun_rad_diff'] = "No Sun in ground truth"
 
-            if tokens[1] is not None and truth_positions[img_name]['sun'] is not None:
+            if tokens[1].startswith('c') and truth_positions[img_name]['sun'] is not None:
                 pos_sum += (sun_center_offset + sun_radius_diff)
                 pos_count += 1
-            if tokens[2] is not None and truth_positions[img_name]['moon'] is not None:
+            if tokens[2].startswith('c') and truth_positions[img_name]['moon'] is not None:
                 pos_sum += (moon_center_offset + moon_radius_diff)
                 pos_count += 1
                 
