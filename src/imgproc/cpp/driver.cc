a70d7d075e5cf45dd9f95b440d6af5a19994a767
Bret Lorimore <bretlorimore@gmail.com>
2017-05-02 03:20:26 +0000

    Pipeline using erosion/dilation in preprocessing

--- src/imgproc/cpp/driver.cc
+++ src/imgproc/cpp/driver.cc
@@ -1,15 +1,13 @@
 #include "imgproc_pipeline_base.h"
+#include "dilation_pipeline.h"
 
-// #include "derived_pipeline_ex.h"
 
 int main(int argc, char **argv){
     
     // Create pipeline object, passing in input image file to constructor    
     ImgProcPipelineBase *pipeline;
     
-    pipeline = new ImgProcPipelineBase(argc, argv);
-    // pipeline = new DerivedPipelineEx(argc, argv); 
-
+    pipeline = new DilationPipeline(argc, argv);
     pipeline->run_all();
     
     return 0;
